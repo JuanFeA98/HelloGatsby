ce76ca08c24183603b1bbbd384a1b67fc9bfd70f
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-08-14 18:25:23 -0500

    GraphQL!

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
         <p>I'm making this by following the Gatsby Tutorial.</p>    
         <StaticImage
           alt='Image'
-          src='https://cdn.pixabay.com/photo/2021/08/03/11/48/canal-6519196_1280.jpg'
+          src='../images/canal.jpg'
         />
     </Layout>
   )
